422bbc6cbaf478df0d984a0c8be2e72c4ed7c369
Neklas Meyer <ne6068@hs-el.de>
2024-08-11 01:35:27 +0200

    Implemented editing of the base details of a project

--- frontend/src/app/components/user/edit-project/edit-project.component.spec.ts
+++ frontend/src/app/components/user/edit-project/edit-project.component.spec.ts
@@ -1,6 +1,13 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideLuxonDateAdapter } from '@angular/material-luxon-adapter';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+
+import { of } from 'rxjs';
 
 import { EditProjectComponent } from './edit-project.component';
+import { ProjectService } from '@Services/project.service';
 
 describe('Component: EditProjectComponent', () => {
   let component: EditProjectComponent;
@@ -8,7 +15,21 @@ describe('Component: EditProjectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditProjectComponent]
+      imports: [EditProjectComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            data: of({
+              project: null,
+            }),
+          },
+        },
+        ProjectService,
+        provideHttpClient(),
+        provideLuxonDateAdapter(),
+        provideAnimations(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EditProjectComponent);
--- frontend/src/app/components/user/edit-project/edit-project.component.ts
+++ frontend/src/app/components/user/edit-project/edit-project.component.ts
@@ -1,12 +1,184 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ActivatedRoute, Data, Router } from '@angular/router';
+
+import { DateTime } from 'luxon';
+import { Subscription, take } from 'rxjs';
+
+import { ProjectMember } from '@Models/project-member';
+import { ProjectMilestone } from '@Models/project-milestone';
+import { Project } from '@Models/project';
+import { UndefinedStringPipe } from '@Pipes/undefined-string.pipe';
+import { DateService } from '@Services/date.service';
+import { SnackbarMessage, SnackbarService } from '@Services/snackbar.service';
+import { ProjectService } from '@Services/project.service';
+import { DeepPartial, Nullable, Undefinable } from '@Types';
+import { HttpException } from '@Utils/http-exception';
+import { FormValidators } from '@Validators';
+
+type RouteData = Data & {
+  project?: Project;
+};
 
 @Component({
   selector: 'hsel-edit-project',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    UndefinedStringPipe,
+    MatTooltipModule,
+  ],
   templateUrl: './edit-project.component.html',
   styleUrl: './edit-project.component.scss'
 })
-export class EditProjectComponent {
+export class EditProjectComponent implements OnInit, OnDestroy {
+  form!: FormGroup; // Will be initialized inside #ngOnInit
+  milestones: ProjectMilestone[] = [];
+  companions: ProjectMember[] = [];
+  students: ProjectMember[] = [];
+  private intervalChangesSubscription: Undefinable<Subscription>;
+  private startDateChangesSubscription: Undefinable<Subscription>;
+  private projectId!: string; // Will be initialized inside #ngOnInit
 
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly formBuilder: FormBuilder,
+    private readonly date: DateService,
+    private readonly snackbar: SnackbarService,
+    private readonly project: ProjectService,
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [FormValidators.required]],
+      interval: [7, [FormValidators.required, FormValidators.min(1)]],
+      startDate: [this.date.getToday(), [FormValidators.required]],
+      endDate: ['', [
+        FormValidators.behindDate('startDate'),
+        FormValidators.matchesInterval('startDate', 'interval'),
+      ]],
+      type: [''],
+    });
+
+    this.intervalChangesSubscription = this.form.get('interval')?.valueChanges
+      .subscribe((_) => {
+        const control = this.form.get('endDate');
+
+        if (control?.dirty) {
+          control.updateValueAndValidity();
+        }
+      });
+
+    this.startDateChangesSubscription = this.form.get('startDate')?.valueChanges
+      .subscribe((_) => {
+        const control = this.form.get('endDate');
+
+        if (control?.dirty) {
+          control.updateValueAndValidity();
+        }
+      });
+
+    this.activatedRoute.data.pipe(take(1)).subscribe(({ project }: RouteData) => {
+      if (!project) return;
+
+      this.projectId = project.id;
+
+      this.form.patchValue({
+        name: project.name ?? '',
+        interval: project.reportInterval ?? 7,
+        startDate: project.officialStart ? DateTime.fromSQL(project.officialStart) : this.date.getToday(),
+        endDate: project.officialEnd ? DateTime.fromSQL(project.officialEnd) : '',
+        type: project.type ?? '',
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalChangesSubscription) {
+      this.intervalChangesSubscription.unsubscribe();
+    }
+
+    if (this.startDateChangesSubscription) {
+      this.startDateChangesSubscription.unsubscribe();
+    }
+  }
+
+  boundFilterEndDate = this.filterEndDate.bind(this);
+
+  private filterEndDate(date: DateTime | null): boolean {
+    if (!date) return false;
+
+    const interval = parseInt(this.form.get('interval')?.value ?? 1);
+    const start: DateTime = this.form.get('startDate')?.value ?? this.date.getToday();
+    const diff = this.date.compare(date.toFormat('yyyy-MM-dd'), start.toFormat('yyyy-MM-dd'));
+
+    if (diff <= 0) return false;
+
+    return diff % interval === 0;
+  }
+
+  onCancelClick(): void {
+    window.history.back();
+  }
+
+  onSubmitClick(): void {
+    const payload: DeepPartial<Project> = {
+      name: this.form.get('name')!.value,
+      reportInterval: this.form.get('interval')!.value,
+      officialStart: this.form.get('startDate')!.value.toFormat('yyyy-MM-dd'),
+      officialEnd: this.getOfficialEnd(),
+      type: this.getProjectType(),
+    };
+
+    this.project.update(':id', this.projectId, payload).pipe(take(1)).subscribe({
+      next: (updatedSuccessfully) => {
+        if (updatedSuccessfully) {
+          this.snackbar.showInfo('Projekt erfolgreich aktualisiert');
+          this.router.navigateByUrl(`/projects/${this.projectId}`);
+        } else {
+          this.snackbar.showWarning('Die Aktualisierung konnte nicht bestätigt werden');
+        }
+      },
+      error: (exception: HttpException) => {
+        this.snackbar.showException(SnackbarMessage.SAVE_OPERATION_FAILED, exception);
+      },
+    });
+  }
+
+  private getOfficialEnd(): Nullable<string> {
+    if (this.form.get('endDate')?.value) {
+      if (this.form.get('endDate')!.value instanceof DateTime) {
+        return this.form.get('endDate')!.value.toFormat('yyyy-MM-dd');
+      }
+    }
+
+    return null;
+  }
+
+  private getProjectType(): Nullable<string> {
+    if (this.form.get('type')?.value) {
+      if (this.form.get('type')!.value.length > 0) {
+        return this.form.get('type')!.value;
+      }
+    }
+
+    return null;
+  }
 }
